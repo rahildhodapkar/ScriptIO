e2b8dc580bed8bc24bfc3ab63e0c9db1930e426d
Rahil Dhodapkar <rdhodapkar@rocketpharma.com>
2023-10-07 12:44:45 -0400

    first commit

---
+++ src/main/java/com/ml/epicfuntime/EpicfuntimeApplication.java
@@ -0,0 +1,13 @@
+package com.ml.epicfuntime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicfuntimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicfuntimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ml/epicfuntime/EpicfuntimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ml.epicfuntime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicfuntimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
