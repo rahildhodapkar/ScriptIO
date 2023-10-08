b8f2cbad50ebb9730517a8b86ec0fb7fc9121fe1
Rahil Dhodapkar <rdhodapkar@rocketpharma.com>
2023-10-07 23:29:37 -0400

    fix canvas script v2

--- src/main/resources/static/canvas.js
+++ src/main/resources/static/canvas.js
@@ -1,22 +1,18 @@
-
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 const clearBtn = document.getElementById("clearBtn");
 const undoBtn = document.getElementById("undoBtn");
-const execute_btn = document.getElementById("execute")
-const execute_draw = document.getElementById("draw")
-const execute_erase = document.getElementById("erase")
-event_click = false
-let timeoutId
+const execute_btn = document.getElementById("execute");
+const execute_draw = document.getElementById("draw");
+const execute_erase = document.getElementById("erase");
+
+let timeoutId = null; // Initialize timeoutId to null
 let painting = false;
-let lastX, lastY;
 const drawingHistory = [];
 
 // Set up initial drawing properties
 context.lineWidth = 2;
 context.strokeStyle = "black";
-
-
 
 // Event listeners
 canvas.addEventListener("mousedown", startPaint);
@@ -27,60 +23,35 @@ clearBtn.addEventListener("click", clearCanvas);
 undoBtn.addEventListener("click", undoLastStroke);
 execute_btn.addEventListener("click", execute_process);
 
-execute_draw.addEventListener("click",execute_draw_f)
-execute_erase.addEventListener("click",execute_erase_f)
+execute_draw.addEventListener("click", execute_draw_f);
+execute_erase.addEventListener("click", execute_erase_f);
 
 // Functions for drawing
 function startPaint(event) {
-    console.log("hello")
-    if(timeoutId){
-        clearTimeout(timeoutId)
+    console.log("hello");
+    if (timeoutId) {
+        clearTimeout(timeoutId); // Clear the existing timeout if any
+        timeoutId = null; // Reset timeoutId to null
     }
-    event_click =true
     painting = true;
     draw(event);
 }
 
 function endPaint() {
-    
-
     painting = false;
-
     context.beginPath();
-    // Save the current drawing state to the history
     saveDrawingState();
-    const startdate = new Date()
-    event_click =false
-    var newdate = new Date()
-    diff = newdate-startdate
 
-    timeoutId = setTimeout(function(){
-        execute_process()
-    },10000)
-    // while (((diff) % (1000 * 60))/ 1000 < 3){
-    //     if (event_click == true){
-    //         break;
-    //     }
-    //     console.log(((diff) % (1000 * 60))/ 1000)
-    //     newdate = new Date()
-    //     diff = newdate-startdate
-
-    // }
-    execute_process()
-
-
-
-    
-
-
-    
+    // Set a timeout to call execute_process after 3 seconds
+    timeoutId = setTimeout(function() {
+        execute_process();
+        timeoutId = null; // Reset timeoutId to null
+    }, 1700); // 3000 milliseconds = 3 seconds
 }
 
 function draw(event) {
     if (!painting) return;
-
     context.lineCap = "round";
-
     context.lineTo(event.clientX - canvas.getBoundingClientRect().left, event.clientY - canvas.getBoundingClientRect().top);
     context.stroke();
     context.beginPath();
@@ -98,7 +69,6 @@ function saveDrawingState() {
 }
 
 function undoLastStroke() {
-
     if (drawingHistory.length > 0) {
         drawingHistory.pop(); // Remove the last saved state
         context.clearRect(0, 0, canvas.width, canvas.height);
@@ -106,27 +76,21 @@ function undoLastStroke() {
             context.putImageData(drawingHistory[drawingHistory.length - 1], 0, 0); // Restore the previous state
         }
     }
-
 }
 
-function execute_process(){
+function execute_process() {
     const pngDataURL = canvas.toDataURL('image/png');
     const pngImage = new Image();
     pngImage.src = pngDataURL;
-    clearCanvas()
-
-    
+    clearCanvas();
 }
 
-function execute_draw_f(){
+function execute_draw_f() {
     context.lineWidth = 2;
     context.strokeStyle = "Black";
 }
-function execute_erase_f(){
+
+function execute_erase_f() {
     context.lineWidth = 10;
     context.strokeStyle = "White";
-
-
 }
-
-
